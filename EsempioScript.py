7dd8bf68632fe12b99243a17d8fc365721095fab
bigdive <bigdive@top-ix.org>
2016-10-07 13:35:40 +0200

    Versionamento prima di pranzo

--- EsempioScript.py
+++ EsempioScript.py
@@ -1,8 +1,12 @@
 #esempio di import di un'altra libreria
-from Hello import hello_world
+from Hello import hello_world, hello_world_ret #se voglio importare tutto usare l'asterisco
 
 print "Hello world"
 
 print "ESEGUO LA FUNZIONE DI SEGUITO"
 
 hello_world()
+
+print "ORA PROVO CON LA FUNZIONE CHE MI RITORNA UN VALORE"
+
+print(hello_world_ret())
--- Hello.py
+++ Hello.py
@@ -1,2 +1,5 @@
 def hello_world():
 	print("Hello")
+
+def hello_world_ret():
+	return "Hello returned"
